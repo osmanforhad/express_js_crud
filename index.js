94454c6d9d88a88735fe84f1d5014f69a3a44bbd
osmanforhad <osmanforhad93@gmail.com>
2025-03-28 21:56:28 +0600

    connect express app with mongo db

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const DbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL).then(() => {
+      console.log("Database Connection Successfull");
+      app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port: ${process.env.PORT}`);
+      });
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+DbConnection();
