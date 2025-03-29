ac005e1587655b5da9033a23e057c8beb57e13a9
osmanforhad <osmanforhad93@gmail.com>
2025-03-30 05:56:45 +0600

    setup success message as a response insted of user input

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,8 @@ export const createUser = async (req, res) => {
     } else {
       //save user input into db
       const saveData = await newUser.save();
-      res.status(200).json(saveData);
+      // res.status(200).json(saveData);
+      res.status(200).json({ message: "User created successfully" });
     }
   } catch (error) {
     res.status(500).json({ errorMessage: error.message });
